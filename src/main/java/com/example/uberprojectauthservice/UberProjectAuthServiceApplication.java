0b2561969c8e537bb667deaf579cbe5063c658cc
Sanket Singh <singhsanket143@gmail.com>
2024-04-26 21:04:50 +0530

    First commit

---
+++ src/main/java/com/example/uberprojectauthservice/UberProjectAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberprojectauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberProjectAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberProjectAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uberprojectauthservice/UberProjectAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uberprojectauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
